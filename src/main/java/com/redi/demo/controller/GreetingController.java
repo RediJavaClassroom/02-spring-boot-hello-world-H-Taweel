e32723ac09eba7375e91092278f56f4d6fae4e45
H-Taweel <hisham.taweel@gmail.com>
2022-03-19 13:56:39 +0100

    Created Greeting class

--- src/main/java/com/redi/demo/controller/GreetingController.java
+++ src/main/java/com/redi/demo/controller/GreetingController.java
@@ -1,13 +1,23 @@
 package com.redi.demo.controller;
 
+import com.redi.demo.domain.Greeting;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
 
-    @GetMapping("greeting")
-    public String greet() {
-        return "Hello, World!";
+    private AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greet(@RequestParam(value = "name", defaultValue = "World") String name) {
+        String content = "Hello " + name + "!";
+        // save stats for the name then increment counter
+        return new Greeting(counter.incrementAndGet(), content);
     }
+
+    // call method of stats
 }
